32bb9ac4fffe17dd8da2995c3539d51d3d46efe2
victorvm77 <vmgutierrez@ucsd.edu>
2022-10-13 17:16:19 -0700

    0

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,23 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceTwo(){
+    int[] input1 = { 44,33,12 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 12 , 33, 44}, input1);
+  }
+
+  @Test 
+  public void testReversedTwo(){
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+  @Test 
+  public void testAverageWithoutLowest(){
+    double[] input1 = {9.0, 9.0, 1.0, 9.0, 9.0};
+
+    assertEquals(9.0, ArrayExamples.averageWithoutLowest(input1), 0.1);
+  }
 }
